ca949764ae5f02b8da0e21190cfc30d6536005c6
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-03-16 12:19:23 +0900

    TeamのeditはTeamのオーナーのみができるよう実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :only_owner, only: %i[edit update]
 
   def index
     @teams = Team.all
@@ -55,5 +56,11 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def only_owner
+    unless current_user.id == Team.friendly.find(params[:id]).owner_id
+      redirect_to team_url(params[:id]), notice: '権限がありません'
+    end
   end
 end
